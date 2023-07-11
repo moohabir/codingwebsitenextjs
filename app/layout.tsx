4331d05d0812e315e54592a834c2e7b40f791b39
moohabir <moohabir@gmail.com>
2023-07-11 23:23:05 +0200

    dvb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={''}>{children}</body>
+      <body className={''}>
+        <Navbar /> {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="flex justify-center items-center gap-20">
+        <p>Home</p>
+        <p>About</p>
+        <p>Courses</p>
+        <p>Profile</p>
+        <p>Login</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
