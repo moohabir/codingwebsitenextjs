f16218d0e6bd6c1bba0b7ed49d2919a2a6ea1658
moohabir <moohabir@gmail.com>
2023-07-11 22:47:57 +0200

    egg

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import VideoCodeEditor from '../components/VideoCodeEditor';
 //import Ide from '@/components/AceEditor';
 
 const HomePage = () => {
-  const videoUrl = 'https://example.com/video.mp4'; // Replace with your actual video URL
+  //const videoUrl = 'https://example.com/video.mp4'; // Replace with your actual video URL
 
   return (
     <div className="flex justify-center flex-col p-4 ">
       <h1 className="text-center">Welcome to My Coding Website</h1>
-      <VideoCodeEditor videoUrl={videoUrl} />
+      {/*<VideoCodeEditor videoUrl={videoUrl} />*/}
+      <VideoCodeEditor />
+      {/* Other content */}
       {/* Other content */}
       {/*<Ide />*/}
       <MonacoEditor />
